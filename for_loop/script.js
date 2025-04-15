22b828e42eaa2ba7bf9371244bdbce48d32ad5b2
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-04-15 18:17:02 +0530

    Add files via upload

---
+++ for_loop/script.js
@@ -0,0 +1,121 @@
+
+//Q1. 2,1,1/2,1/4,1/8
+
+
+// document.writeln("2 1");
+// for( let i=2;i<=10;i*=2){
+//    document.writeln("1/"+i);
+  
+// }
+
+
+//Q2. 7 10 8 11 9 12 10
+
+// let a=7;
+// let b=10
+// for(let i=1; i<=3;i++){
+//    document.writeln(a+ " ",+b)
+// a++;
+// b++;
+// }
+
+
+//Q3. 36 34 30 28 24 22
+
+// let a=36;
+// let b=34;
+// for(let i=1; i<=3;i++){
+//  document.writeln(a+ " " +b);
+//  a-=6;
+//  b-=6;
+// }
+
+
+//Q4. 22 21 23 22 24 23
+
+// -1, +2
+
+// let a =22;
+// let b=21;
+// for(let i=1; i<=4;i++){
+//     document.writeln(a+ " " +b);
+//  a+=1;
+//  b+=1;
+// }
+
+
+// Q5. 53 53 40 40 27 27 14 14
+
+// let a = 53;
+// for(let i=1; i<=4;i++){
+// document.writeln(a+" " +a);
+// a-=13;
+// }
+
+//Q6. 21 9 21 11 21 13 21 15
+
+// let a=21;
+// let b=9;
+// for(let i=1;i<=4;i++){
+//   document.writeln(a+ " "+b);
+//   b+=2;
+// }
+
+
+// Q7. 52 46 40 34 28
+
+// let a=52;
+// for(let i=1;i<=5;i++){
+// document.writeln(a);
+// a-=6;
+// }
+
+
+
+//Q8. 3 4 7 8 11 12
+
+// let a=3;
+// let b=4;
+// for(let i=1;i<=3;i++){
+// document.writeln(a+ " "+b);
+// a+=4;
+// b+=4;
+// }
+
+// Q9. 8 22 8 28 8 34
+
+// let a=8;
+// let b=22;
+// for(let i=1;i<=3;i++){
+// document.writeln(a+ " "+b);
+// b+=6;
+// }
+
+
+//Q10. 31 29 24 22 17 15
+// let a=31;
+// let b=29;
+// for(let i=1; i<=3;i++){
+// document.writeln(a+ " " +b);
+// a-=7;
+// b-=7;
+// }
+
+
+//Q11. 1.5  2.3  3.1  3.9  4.7
+
+//  let a=1.5;
+//  let b=2.3;
+//  for(let i=1;i<=3;i++){
+//      document.writeln(a+ " " +b);
+//      a+=1.6;
+//      b+=1.6;
+//  }
+
+//trying second method
+
+// let a =15;
+// for(let i=1;i<=5;i++){
+// document.writeln(a);
+// a=(a+8)/10;
+// }
