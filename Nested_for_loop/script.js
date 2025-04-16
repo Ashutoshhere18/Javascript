80987cf869a72a99a9777deb5be1285340b7ca9a
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-04-16 16:38:49 +0530

    Add files via upload

---
+++ Nested_for_loop/script.js
@@ -0,0 +1,73 @@
+
+// Q1. pattern : 
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+
+// for(let i=1;i<=4;i++){//outerloop
+//     for(let j=1;j<=5;j++){//inner loop
+// document.writeln(j);
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Q2. pattern:
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+// for(let i=1;i<=4;i++){//outerloop
+//     for(let j=1;j<=5;j++){//inner loop
+// document.writeln("*");
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Q3. pattern :
+// 1 1 1 1 1
+// 2 2 2 2 2
+// 3 3 3 3 3
+// 4 4 4 4 4 
+
+// for(let i=1;i<=4;i++){//outerloop
+//     for(let j=1;j<=5;j++){//inner loop
+// document.writeln(i);
+//     }
+//     document.writeln("<br>");
+// }
+
+// Q4. pattern :
+// 1-3-5
+// 1-3-5
+// 1-3-5
+// 1-3-5
+
+// for(let i=1;i<=4;i++){
+//     for(let j=1;j<=5;j+=2){
+//        document.writeln(j+ "-");
+//     }
+//     document.writeln("<br>")
+// }
+
+
+//Q5. pattern :
+// 1 0 1 0 1
+// 1 0 1 0 1
+// 1 0 1 0 1
+// 1 0 1 0 1
+
+// for(let i=1;i<=4;i++){//outerloop
+//         for(let j=1;j<=5;j++){//inner loop
+//     if(j%2==0){
+// document.writeln("0");
+//     }else{
+// document.writeln("1");
+//     }
+//         }
+//         document.writeln("<br>");
+//     }
+    
