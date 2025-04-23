98d6bb6b72dc7685fcd5e8bb4c3faaa244a3df42
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-04-23 16:19:41 +0530

    Delete If_else_day-1 directory

--- If_else_day-1/Program_5.js
+++
@@ -1,9 +0,0 @@
-// Write a program to check the number is even or not ,using if else .
-
-let num=7;
-if(num%2==0){
-    document.writeln("Number is even")
-}
-else{
-    document.writeln("Number is odd")
-}
