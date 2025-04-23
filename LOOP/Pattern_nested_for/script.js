a8e3e8d913b5855f7091fafb5c0db3786d0df7f0
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-04-23 16:25:57 +0530

    Add files via upload

---
+++ LOOP/Pattern_nested_for/script.js
@@ -0,0 +1,375 @@
+//  common Pattern of *
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// document.writeln("*");
+//     }
+//     document.writeln("<br>");
+// }
+
+//Printing A using the above pattern
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+//         if(j==1 ||j==4||i==3||i==1){
+//             document.writeln("*");
+//         }else{
+//             document.writeln("&nbsp &nbsp");
+//         }
+
+//     }
+//     document.writeln("<br>");
+// }
+
+// printing B
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1||(i==1&&j==2)||(i==1&&j==3)||(i==4&&j==2)||(i==4&&j==3)||(j==4&&i==2)||(j==4&&i==6)||(j==3&&i==3)||(j==3&&i==5)||(i==7&&j==2)||(i==7&&j==3)){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing C
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1||(j==2&&i==1)||(j==2&&i==7)||(j==3&&i==1)||(j==3&&i==7)||(j==4&&i==1)||(j==4&&i==7)){
+//     document.writeln("*");
+// }else{
+//     document.writeln();
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Printing D
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1||(j==2&&i==1)||(j==2&&i==7)||(j==3&&i==2)||(j==3&&i==6)||(j==4&&i==3)||(j==4&&i==5)||(j==5&&i==4)){
+//      document.writeln("*");
+// }else{
+// document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing E
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=4;j++){
+// if(j==1||i==1||i==7||i==4){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing F
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=4;j++){
+// if(j==1||i==1||i==4){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing G
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if((i==1&&(j==2||j==3||j==4))||(i==2&&(j==1||j==5))||(i==3&&j==1)||(i==4&&(j==1||j==3||j==4||j==5))||(i==5&&(j==1||j==5))||(i==6&&(j==1||j==5))||(i==7&&(j==2||j==3||j==4))){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing H
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=4;j++){
+// if(j==1||j==4||i==4){
+//     document.writeln("*");
+// }else{
+// document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing I
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+//   if(i==1||i==7||j==3){
+// document.writeln("*");
+//   }else{
+// document.writeln("&nbsp");
+//   }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing J
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=6;j++){
+// if((j==1&&i==6)||j==2&&(i==1||i==7)||(j==3&&i==1)||j==4||(j==5&&i==1)||(j==6&&i==1)){
+// document.writeln("*");
+// }else{
+// document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Printing K
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1||(j==2&&i==4)||j==3&&(i==3||i==5)||j==4&&(i==2||i==6)||j==5&&(i==1||i==7)){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing L
+
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=4;j++){
+// if(j==1||i==7){
+//     document.writeln("*");
+// }
+// else{
+//     document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Printing M
+
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1||j==5||(j==2&&i==2)||(j==3&&i==3)||(j==4&&i==2)){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp ");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Printing N
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1||j==5||(j==2&&i==3)||(j==3&&i==4)||(j==4&&i==5)){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Print O
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if((i==1&&(j==2||j==3||j==4))||(i==2&&(j==1||j==5))||i==3&&(j==1||j==5)||(i==4&&(j==1||j==5))||(i==5&&(j==1||j==5))||(i==6&&(j==1||j==5))||(i==7&&(j==2||j==3||j==4))){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Print P
+
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1||(i==1&&j==2)||(i==1&&j==3)||(i==1&&j==4)||(i==4&&j==2)||(i==4&&j==3)||(i==4&&j==4)||(j==5&&i==2)||(j==5&&i==3)){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+
+//Print Q
+
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=6;j++){
+// if((i==1&&(j==2||j==3||j==4))||(i==2&&(j==1||j==5))||i==3&&(j==1||j==5)||(i==4&&(j==1||j==5))||(i==5&&(j==1||j==5||j==3))||(i==6&&(j==1||j==4))||(i==7&&(j==2||j==3||j==6))){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Print R
+
+
+//Print S
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1&&(i==2||i==3||i==7)||j==2&&(i==1||i==4||i==7)||j==3&&(i==1||i==4||i==7)||j==4&&(i==1||i==4||i==7)||j==5&&(i==1||i==5||i==6)){
+// document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+    
+//     }
+// }
+//     document.writeln("<br>");
+// }
+
+
+//Print T
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(i==1||j==3){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp");
+    
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+
+// Print U
+
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(i==1&&(j==1||j==5)||i==2&&(j==1||j==5)||i==3&&(j==1||j==5)||i==4&&(j==1||j==5)||i==5&&(j==1||j==5)||i==6&&(j==1||j==5)||i==7&&(j==2||j==3||j==4)){
+// document.writeln("*");
+// }else{
+// document.writeln("&nbsp &nbsp")
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//Print V
+
+
+// for(let i=1;i<=6;i++){
+//     for(let j=1;j<=5;j++){
+// if(i==1&&(j==1||j==5)||i==2&&(j==1||j==5)||i==3&&(j==1||j==5)||i==4&&(j==1||j==5)||i==5&&(j==2||j==4)||i==6&&j==3){
+// document.writeln("*");
+// }else{
+// document.writeln("&nbsp &nbsp")
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Print W
+
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=5;j++){
+// if(j==1||j==5||(j==2&&i==6)||(j==3&&i==5)||(j==4&&i==6)){
+//     document.writeln("*");
+// }else{
+//     document.writeln("&nbsp &nbsp");
+    
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Print X
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=7;j++){
+// if(j==1&&(i==1||i==7)||j==2&&(i==2||i==6)||j==3&&(i==3||i==5)||j==4&&i==4||j==5&&(i==3||i==5)||j==6&&(i==2||i==6)||j==7&&(i==1||i==7)){
+// document.writeln("*")
+// }else{
+// document.writeln("&nbsp&nbsp")
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+
+//Print Y
+
+// for(let i=1;i<=7;i++){
+//     for(let j=1;j<=7;j++){
+// if(i==1&&(j==1||j==7)||i==2&&(j==2||j==6)||i==3&&(j==3||j==5)||(i==4&&j==4)||(i==5&&j==4)||(i==6&&j==4)||(i==7&&j==4)){
+//     document.writeln("*")
+// }else{
+//     document.writeln("&nbsp&nbsp")
+// }
+//     }
+//     document.writeln("<br>");
+// }
+
+
+// Print Z
+
+
+for(let i=1;i<=7;i++){
+    for(let j=1;j<=5;j++){
+   if(i==1||i==7||(i==2&&j==6)||(i==3&&j==5)||(i==4&&j==4)||(i==5&&j==3)||(i==6&&j==2)){
+document.writeln("*");
+   }else{
+document.writeln("&nbsp");
+   }
+    }
+    document.writeln("<br>");
+}
---
+++ LOOP/for_loop/script.js
@@ -0,0 +1,199 @@
+
+//Q1. 2,1,1/2,1/4,1/8
+
+
+// document.writeln("2 1");
+// for( let i=2;i<=10;i*=2){
+//    document.writeln("1/"+i);
+  
+// }
+
+
+//Q2. 7 10 8 11 9 12 10
+
+// let a=7;
+// let b=10
+// for(let i=1; i<=3;i++){
+//    document.writeln(a+ " ",+b)
+// a++;
+// b++;
+// }
+
+
+//Q3. 36 34 30 28 24 22
+
+// let a=36;
+// let b=34;
+// for(let i=1; i<=3;i++){
+//  document.writeln(a+ " " +b);
+//  a-=6;
+//  b-=6;
+// }
+
+
+//Q4. 22 21 23 22 24 23
+
+// -1, +2
+
+// let a =22;
+// let b=21;
+// for(let i=1; i<=4;i++){
+//     document.writeln(a+ " " +b);
+//  a+=1;
+//  b+=1;
+// }
+
+
+// Q5. 53 53 40 40 27 27 14 14
+
+// let a = 53;
+// for(let i=1; i<=4;i++){
+// document.writeln(a+" " +a);
+// a-=13;
+// }
+
+//Q6. 21 9 21 11 21 13 21 15
+
+// let a=21;
+// let b=9;
+// for(let i=1;i<=4;i++){
+//   document.writeln(a+ " "+b);
+//   b+=2;
+// }
+
+
+// Q7. 52 46 40 34 28
+
+// let a=52;
+// for(let i=1;i<=5;i++){
+// document.writeln(a);
+// a-=6;
+// }
+
+
+
+//Q8. 3 4 7 8 11 12
+
+// let a=3;
+// let b=4;
+// for(let i=1;i<=3;i++){
+// document.writeln(a+ " "+b);
+// a+=4;
+// b+=4;
+// }
+
+// Q9. 8 22 8 28 8 34
+
+// let a=8;
+// let b=22;
+// for(let i=1;i<=3;i++){
+// document.writeln(a+ " "+b);
+// b+=6;
+// }
+
+
+//Q10. 31 29 24 22 17 15
+// let a=31;
+// let b=29;
+// for(let i=1; i<=3;i++){
+// document.writeln(a+ " " +b);
+// a-=7;
+// b-=7;
+// }
+
+
+//Q11. 1.5  2.3  3.1  3.9  4.7
+
+//  let a=1.5;
+//  let b=2.3;
+//  for(let i=1;i<=3;i++){
+//      document.writeln(a+ " " +b);
+//      a+=1.6;
+//      b+=1.6;
+//  }
+
+
+
+//Q12. 14 28 20 40 32 64
+
+// let a=14;
+// document.writeln("14")
+// for(let i=1;i<=3;i++){
+
+// for(let j=1;j<=1;j++){
+   
+//    a*=2;
+//    document.writeln(a);
+// }
+// a-=8;
+// document.writeln(a);
+// }
+
+
+//Q13. 2 4 6 8 10 12
+
+// let a=2;
+// for(let i=1;i<=6;i++){
+//   document.writeln(a);
+//   a+=2;
+// }
+
+
+//Q14. 201 202 204 207 211
+
+// let n=201;
+// for(let i=1;i<=5;i++){
+// document.writeln(n);
+// n+=i;
+// }
+
+
+//Q15. 544 509 474 439 404
+
+// let a =544;
+// for(let i=1;i<=5;i++){
+// document.writeln(a);
+// a-=35;
+// }
+
+
+
+//Q16. 80 10 70 15 60 20
+
+// let a=80;
+// let b=10;
+// for(let i=1;i<=3;i++){
+// document.writeln(a+" "+b);
+// a-=10;
+// b+=5;
+// }
+
+
+//Q17. 2 6 18 54 162
+
+// let a=2;
+// for(let i=1;i<=5;i++){
+// document.writeln(a);
+// a*=3;
+// }
+
+
+//Q18. 5.2  4.8  4.4  4  3.6
+
+// let a=5.2;
+// for(let i=1;i<=5;i++){
+//    document.writeln(a);
+//    a-=0.4;
+// }
+
+
+
+//Q19. 8 6 9 23 87 429
+
+// let a=8;
+
+// for(let i=1;i<=6;i++){
+//    document.writeln(a);
+//    a= a*i-(i+1);
+  
+// }
