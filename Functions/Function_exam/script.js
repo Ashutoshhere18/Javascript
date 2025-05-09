8bf58029357049bad105552bf4d81f39b992f69c
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-05-09 15:19:51 +0530

    Add files via upload

---
+++ Functions/Function_exam/script.js
@@ -0,0 +1,37 @@
+
+//WAP TO FIND FACTORIAL USING RECURSION.
+// let n=parseInt(prompt("Enter the evalue of number to find factorial:"));
+//  function factorial(n){
+//       if(n==1){
+//         return 1;
+//       }
+//       return n* factorial(n-1);
+//  }
+
+//  document.writeln(factorial(n));
+
+
+
+
+
+//WAP TO CHECK WHETHER A NUMBER IS A PALINDROME OR NOT USING A FUNCTION.
+// let num=parseInt(prompt("Enter number to check palindrome or not:"));
+// let temp=num;
+// function palindrome(num){//121
+//     let reverse=0;
+//     while(temp>0){
+//         let ld=temp%10;
+//         let rev=ld;
+//      reverse=(reverse*10)+rev;
+//        temp=Math.floor(temp/=10);
+//     }
+//    if(num==reverse){
+//     document.writeln("The entered number is palindrome.");
+//    }
+//    else{
+//     document.writeln("The entered number is not palindrome.");
+//    }
+
+// }
+
+// palindrome(num);
