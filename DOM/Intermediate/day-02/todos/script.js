f7f833bce817b6f80e175040c70fec9e219e574e
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-06-10 15:10:21 +0530

    Update script.js

--- DOM/Intermediate/day-02/todos/script.js
+++ DOM/Intermediate/day-02/todos/script.js
@@ -3,32 +3,53 @@ const input = document.getElementById("input");
 const button = document.getElementById("button");
 
 const outputBox = document.getElementById("output-box");
-let todos=["be happy","be kind to others","At last give smile and move"];
-function todosdisplay(){
-    
-todos.forEach((todo)=>{
-    
+let todos = ["be happy", "be kind to others", "At last give smile and move"];
+function todosdisplay() {
+  outputBox.innerHTML = "";
+  todos.forEach((todo, index) => {
     const value = input.value;
-  const div = document.createElement("div");
-  div.className = "d-flex justify-content-between my-3";
+    const div = document.createElement("div");
+    const btnDiv=document.createElement("div");
+    div.className = "d-flex justify-content-between my-3";
+    
+    const p = document.createElement("p");
+    const delBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
 
-  const p = document.createElement("p");
-  const delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.className = "btn btn-danger";
 
-  delBtn.textContent = "Delete";
-  delBtn.className = "btn btn-danger";
-  p.textContent = todo;
+    editBtn.textContent = "Edit";
+    editBtn.className = "btn btn-success mx-3";
 
-  div.appendChild(p);
-  div.appendChild(delBtn);
+    p.textContent = todo;
 
-  outputBox.appendChild(div);
-});
+    function deltodos(index) {
+      todos.splice(index, 1);
+      todosdisplay();
+    }
+
+    function editTodos(index){
+     input.value=todos[index];
+    }
+
+    delBtn.addEventListener("click", () => {
+      deltodos(index);
+    });
+
+    editBtn.addEventListener('click',()=>{
+      editTodos(index);
+      todos.splice(index,1);
+    })
+    div.appendChild(p);
+    btnDiv.appendChild(delBtn);
+    btnDiv.appendChild(editBtn);
+     div.appendChild(btnDiv);
+    outputBox.appendChild(div);
+  });
 }
 
-
 button.addEventListener("click", () => {
-  outputBox.innerHTML="";
   const value = input.value;
   todos.push(value);
   todosdisplay();
